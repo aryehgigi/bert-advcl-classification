60a135bdb5e6b544457b7a28057fba9c99351c6e
aryehgigi <aryehgigi@gmail.com>
2020-04-12 02:13:33 +0300

    bugs7

--- model.py
+++ model.py
@@ -13,9 +13,11 @@ class AdvclTransformer(torch.nn.Module):
         self.hidden_size = self.input_size  # TODO (use config)
         
         # added model  # TODO - revisit
-        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.fc1 = torch.nn.Linear(self.input_size, int(self.hidden_size/2))
         self.relu = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+        self.fc2 = torch.nn.Linear(int(self.hidden_size/2), 1)
+        self.relu2 = torch.nn.ReLU()
+        self.fc3 = torch.nn.Linear(2, 1)
         self.sigmoid = torch.nn.Sigmoid()
         
         self.transformer = base_model
@@ -25,21 +27,21 @@ class AdvclTransformer(torch.nn.Module):
         self.class_weights = class_weights
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, args_indices=None):
-        transformer_outputs = self.transformer(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)  # TODO
+        transformer_outputs = self.transformer(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
         output = transformer_outputs[0]
 
         batched_cat = []
         for i, (arg1_index, arg2_index, predicate_index) in enumerate(args_indices):
-            batched_cat.append(output[i, [arg1_index, arg2_index, predicate_index]].view(-1))  # TODO
+            batched_cat.append(output[i, [arg1_index, arg2_index, predicate_index]])
         output = torch.stack(batched_cat)
 
-        logits = self.sigmoid(self.fc2(self.relu(self.fc1(output))))
+        logits = self.sigmoid(self.fc3(self.relu(self.fc2(self.relu(self.fc1(output)))).squeeze()[:,[0,1]])).squeeze()
 
         outputs = (logits,) + transformer_outputs[2:]
-
+        
         if labels is not None:
             loss_fct = BCELoss(self.class_weights)
-            loss = loss_fct(logits.view(-1), labels.view(-1))  # TODO
+            loss = loss_fct(logits, labels)  # TODO
             outputs = (loss,) + outputs
         
         return outputs
